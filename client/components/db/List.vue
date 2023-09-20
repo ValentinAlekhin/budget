0a66e5b4113ab006b1ddbc9a7595b2d9f86e01e0
Valentin <alekhin.v99@gmail.com>
2023-09-20 17:59:59 +0300

    Обновлена БД для телефонов

--- client/components/db/List.vue
+++ client/components/db/List.vue
@@ -8,20 +8,28 @@
       class="flex items-center justify-between p-1 sm:b-2 px-3 sm:px-4 mb-2 sm:mb-3"
     >
       <div class="flex flex-col justify-between">
-        <div class="flex items-center mb-2">
+        <div class="grid grid-cols-12 items-center mb-2 w-72">
           <span
-            class="flex w-3 h-3 rounded-full mr-2"
+            class="col-span-1 flex w-3 h-3 rounded-full mr-2"
             :class="getTypeBackgroundClasses(row.type)"
           />
+
           <span
-            class="text-sm font-medium text-gray-900 truncate dark:text-white"
+            class="col-span-6 w-32 text-sm font-medium text-gray-900 dark:text-white truncate"
           >
             {{ getCategoryName(row.categoryId) }}
           </span>
+
+          <span
+            class="col-span-4 text-sm text-gray-500 dark:text-gray-400 text-left"
+          >
+            {{ dayjs(row.timestamp).format("DD.MM.YYYY") }}
+          </span>
         </div>
-        <span class="text-sm text-gray-500 truncate dark:text-gray-400">
-          {{ dayjs(row.timestamp).format("DD.MM.YYYY") }}
-        </span>
+
+        <p class="pl-6 pr-2 text-sm text-gray-500 dark:text-gray-400">
+          {{ row.comment }}
+        </p>
       </div>
       <div class="flex flex-col items-end justify-end">
         <span class="text-base font-semibold text-gray-900 dark:text-white">
