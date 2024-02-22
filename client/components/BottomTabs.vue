8deb9b6425ba499e0f1f433bd0c462b9114bbaac
Valentin <alekhin.v99@gmail.com>
2024-02-22 19:04:19 +0300

    Откат

--- client/components/BottomTabs.vue
+++ client/components/BottomTabs.vue
@@ -1,18 +1,33 @@
 <template>
   <div
-    class="fixed bottom-0 z-30 w-full rounded-t-xl border-t border-gray-200 px-6 backdrop-blur dark:border-gray-800"
+    class="bg-background/75 fixed bottom-0 z-30 w-full rounded-t-xl border-t border-gray-200 px-2 pt-2 backdrop-blur dark:border-gray-800"
   >
-    <UHorizontalNavigation
-      :links="horizontalNavigationLinks"
-      class="border-b border-gray-200 dark:border-gray-800"
-      @click="vibrate"
-    />
+    <UTabs
+      :model-value="activeIndex"
+      :default-index="activeIndex"
+      :items="links"
+      class="bg-background/75 w-full backdrop-blur"
+      @change="push"
+    >
+      <template #default="{ item }">
+        <UIcon :name="item.icon" class="h-4 w-4 shrink-0" />
+      </template>
+    </UTabs>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useVibrate } from '@vueuse/core'
+import { useMainLinks } from '~/composables/useMainLinks'
+
 const router = useRouter()
 
-const { horizontalNavigationLinks } = useMainLinks()
+const { links, activeIndex } = useMainLinks()
 const { vibrate } = useVibrate({ pattern: [10] })
+
+const push = (index: number) => {
+  const to = links.value.find((_, i) => i === index)?.to
+  router.push(to)
+  vibrate()
+}
 </script>
--- client/composables/useMainLinks.ts
+++ client/composables/useMainLinks.ts
@@ -1,15 +1,28 @@
 export function useMainLinks() {
-  const links = [
-    { label: 'Costs', icon: 'i-heroicons-banknotes', to: '/' },
-    { label: 'Data', icon: 'i-heroicons-circle-stack', to: '/db' },
-    {
-      label: 'Statistic',
-      icon: 'i-heroicons-presentation-chart-line',
-      to: '/stat',
-    },
-  ]
+  const route = useRoute()
 
-  const horizontalNavigationLinks = links.map((l) => [l])
+  const links = computed(() =>
+    [
+      { name: 'Costs', icon: 'i-heroicons-banknotes', to: '/' },
+      { name: 'Data', icon: 'i-heroicons-circle-stack', to: '/db' },
+      {
+        name: 'Statistic',
+        icon: 'i-heroicons-presentation-chart-line',
+        to: '/stat',
+      },
+    ].map((l) => {
+      let active = false
+      if (route.path === '/' || l.to === '/') {
+        active = l.to === route.path
+      } else {
+        active = route.path.includes(l.to)
+      }
 
-  return { links, horizontalNavigationLinks }
+      return { ...l, active }
+    }),
+  )
+
+  const activeIndex = computed(() => links.value.findIndex((l) => l.active))
+
+  return { links, activeIndex }
 }
